ec77c313f7ff6d052480ea475629974ec49478a1
shannx <sbeshenelle@gmail.com>
2023-11-08 09:29:42 +0800

    feat addeed column price syntax

--- hero.sql
+++ hero.sql
@@ -153,3 +153,7 @@ VALUES
     (8, 8, 108),  -- Fanny has Thunder Belt
     (9, 9, 109),  -- Leomord has Blade Armor
     (10, 10, 110);  -- Chou has Feather of Heaven
+
+ALTER TABLE Item
+ADD COLUMN item_price
+DECIMAL(10,2);
